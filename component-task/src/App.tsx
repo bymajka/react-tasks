3df570e427dbfcedcdd8e9d2e0a6164934d55256
Orest Hlavatskyi <_>
2025-03-04 19:29:54 +0200

    Added header with page title and searchbar

--- component-task/src/App.tsx
+++ component-task/src/App.tsx
@@ -1,6 +1,7 @@
-import TaskForm from "./components/TaskForm"
-import { useState } from "react";
 import { TASKS } from "./constants/tasks";
+import { useState } from "react";
+import Header from "./components/header/Header";
+import TaskForm from "./components/TaskForm"
 import TaskList from "./components/TaskList"
 
 const App = () => {
@@ -8,10 +9,13 @@ const App = () => {
   const [tasks, setTasks] = useState(TASKS);
 
   return (
+    <>
+    <Header />
     <div className="flex flex-row flex-1/2 justify-center bg-neutral-200 gap-4">
       <TaskForm tasks={tasks} setTasks={setTasks} />
       <TaskList tasks={tasks} />
     </div>
+    </>
   )
 }
 
