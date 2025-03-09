75db07b48f18712ef69aec84e4d66a9f6d562c5f
Orest Hlavatskyi <_>
2025-03-09 22:34:01 +0200

    Implemented hook form instead of default onChange envents

--- component-task/src/components/TaskCheckbox.tsx
+++ component-task/src/components/TaskCheckbox.tsx
@@ -1,16 +1,12 @@
-import { ChangeEventHandler } from "react";
-
 interface TaskCheckboxProps {
     htmlFor: string;
     labelText: string;
-    checked: boolean;
-    onChangeEvent: ChangeEventHandler<HTMLInputElement>;
 }
-const TaskCheckbox = (props: TaskCheckboxProps) => {
+const TaskCheckbox = ({htmlFor, labelText, ...rest}: TaskCheckboxProps) => {
     return (
         <div className="flex flex-row gap-2 items-center">
-            <label htmlFor={props.htmlFor} className="text-amber-400 [text-shadow:_0_2px_0_rgb(0_0_0_/_40%)] text-2xl">{props.labelText}</label>
-            <input id="input-completed" checked={props.checked} className="mt-2 w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 outline-0 accent-amber-400" type="checkbox" onChange={props.onChangeEvent}/>
+            <label htmlFor={htmlFor} className="text-amber-400 [text-shadow:_0_2px_0_rgb(0_0_0_/_40%)] text-2xl">{labelText}</label>
+            <input id="input-completed" {...rest} className="mt-2 w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 outline-0 accent-amber-400" type="checkbox"/>
         </div>
     )
 }
